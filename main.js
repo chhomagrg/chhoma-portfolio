54a0e31494c28172330b38d0f67a2d01de1c9530
chhomagrg <112659427+chhomagrg@users.noreply.github.com>
2024-12-17 01:43:32 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+// Automatically show the modal after a 500-millisecond delay
+setTimeout(function () {
+    $('#demo-modal').modal('show');// Triggers the Bootstrap modal with ID 'demo-modal' to appear
+}, 500);
